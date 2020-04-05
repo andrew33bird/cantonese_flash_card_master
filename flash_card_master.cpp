5e166825c6ef9d7bc8b336d7a97c3ca1601c348b
andrew33bird <13815976+andrew33bird@users.noreply.github.com>
2020-04-04 17:10:28 -0700

    0.0.1.5

--- flash_card_master.cpp
+++ flash_card_master.cpp
@@ -1,8 +1,15 @@
 /*******************************************************************************
 ** Project: Chinese Flash Cards
 ** Current Version: 0.0.0.4
-********************************************************************************
-** Version 0.0.0.4 *************************************************************
+*******************************************************************************/
+/* Version 0.0.1.5 *************************************************************
+** 5/27/17 Program name change to flashCardMaster
+    Program takes in arguments of files.  Only searches the files now.  Still
+        inputs from test.txt.
+    Moved the file scan to a separtate function
+    Added error code check function
+*******************************************************************************/
+/* Version 0.0.0.4 *************************************************************
 ** 5/20/17 Fixed two bugs: lines 89 and 174.
     Started testing file read and error codes.
 ********************************************************************************
@@ -13,7 +20,6 @@
 ********************************************************************************
 ** Version 0.0.0.2 *************************************************************
 ** 5/6/17 Rev 0.0.0.2 does not compile.  Variable length array is not in C
-    http://stackoverflow.com/questions/33423502/expression-did-not-evaluate-to-a-constant-c
 ** 5/6/17 Opens test file, counts number of lines and max characters for a 
     Cantonese or English string.  When scanning file, returns different error:
         -1000 Error if can't open file
@@ -31,70 +37,73 @@
 #include <stdlib.h> // exit
 #include <string.h> // strcpy
 
-int main(int argc, char *argv[])
+/* Function takes number of arguments entered with exe.  If just exe, print out
+version number, and usage example. */
+int inputArgumentsCheck(int numberOfArguments, char *version)
+{
+    if (numberOfArguments == 1)
+    {
+        printf("Flash Card Master\n");
+        printf("Version: %s\n", version);
+        printf("\tEnter file names in quotations\n");
+        printf("\tFiles must be in same directory as exe\n");
+        printf("\tExample: flashCardMaster.exe \"file_1.txt\" \"file_2.txt\"\n");
+        exit(0);
+    }
+    return 0;
+}
+
+/* Function takes pointer to file name and searches file for line number and
+max characters per language phrase
+Pointers to max characters per file and number of lines are passed
+Function returns error code - 0 if no error */
+int fileScan(char *fileName, int *maxLine, int *lineNumber)
 {
     FILE *f; // pointer to file
     
-    // Open file.  exit with error code if error.
-    f = fopen("test.txt", "r");
+    f = fopen(fileName, "r");
     if (f == NULL)
-    {
-        printf("Error opening file");
-        exit(-1000);
-    }
+        return -1000;
     
-    int maxChar = 0; // max character for each line of Cantonese or English
-    int numberChar = 0; // number of characters for current line
-    int lineNumber = 0; // number of lines in files
+    int numberOfChars = 0; // number of characters for the current line
     int newLineFlag = 1; // flag when at a new line.  Starts with new line
     int pipeRead = 0; // ensures that a | is read for each line
     char c; // temp char during upload of file
+    *maxLine = 0; // max characters for each line of Cantonese or English
+    *lineNumber = 0; // number of lines in file
     
     while(!feof(f))
     {
         c = fgetc(f);
         if ((c == '\n') || (c == '\r'))
         {
-            if (numberChar > maxChar)
-                maxChar = numberChar;
-            numberChar = 0;
-            lineNumber++;
+            if (numberOfChars > *maxLine)
+                *maxLine = numberOfChars;
+            numberOfChars = 0;
+            *lineNumber++;
             if (newLineFlag == 1)
-            {
-                printf("Error: Empty line in file");
-                exit(-1100);
-            }
+                return -1100;
             else if (pipeRead = 0)
-            {
-                printf("Error: Must have '|' on each line to divide Cantonese and English");
-                exit(-1200);
-            }
+                return -1200;
             else
-            {
                 newLineFlag = 1;
-            }
         }
         else if (c == '|')
         {
-            if (numberChar > maxChar)
-                maxChar = numberChar;
-            numberChar = 0;
+            if (numberOfChars > *maxLine)
+                *maxLine = numberOfChars;
+            numberOfChars = 0;
             if (pipeRead == 1)
-            {
-                printf("Error: 2 '|' characters on the same line");
-                exit(-1300);
-            }
+                return -1300;
             else
-            {
-                pipeRead = 1; // fixed bug in 0.0.0.4
-            }
+                pipeRead = 1;
         }
         else
         {
-            numberChar++;
+            numberOfChars++;
             if (newLineFlag == 1)
             {
-                lineNumber++;
+                *lineNumber++;
                 newLineFlag = 0;
             }
         }
@@ -102,41 +111,74 @@ int main(int argc, char *argv[])
     if (newLineFlag == 0)
     {
         if ((c == '|') && (pipeRead == 1))
-        {
-            printf("Error: 2 '|' characters on the same line");
-            exit(-1300);
-        }
+            return -1300;
         else if ((c != '|') && (pipeRead == 0))
-        {
-            printf("Error: Must have '|' on each line to divide Cantonese and English");
-            exit(-1200);
-        }
+            return -1200;
         else if (c == '|')
         {
-            if (numberChar > maxChar)
-                maxChar = numberChar;
-            lineNumber++;
+            if (numberOfChars > *maxLine)
+                *maxLine = numberOfChars;
+            *lineNumber++;
         }
         else
         {
-            if (numberChar > maxChar)
-                maxChar = numberChar;
-            lineNumber++;
+            if (numberOfChars > *maxLine)
+                *maxLine = numberOfChars;
+            *lineNumber++;
         }
     }
     fclose(f);
     
-    if (lineNumber > 50)
+    return 0;
+}
+
+/* Function takes return code and exits if error */
+int check(int rc)
+{
+    if (rc != 0)
+    {
+        printf("Error: %d\n", rc);
+        exit(rc);
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    char versionString[20];
+    strcpy(versionString, "0.0.1.5");
+    inputArgumentsCheck(argc, versionString);
+    
+    int currentMaxLine = 0; // max characters per line of current file
+    int maxLine = 0; // max characters per line of all scanned files
+    int currentNumOfLines = 0; // number of lines of current file
+    int numOfLines = 0; // number of lines for all scanned files
+    
+    for (int i=1;i<argc;i++)
+    {
+        check(fileScan(argv[i], &currentMaxLine, &currentNumOfLines));
+        if (currentMaxLine > maxLine)
+            maxLine = currentMaxLine;        
+        if (currentNumOfLines > numOfLines)
+            numOfLines = currentNumOfLines;
+    }
+    maxLine++; // needs one for the null character
+    
+    if (numOfLines > 50)
     {
         printf("Total lines must be less than 50\n");
         exit(-1400);
     }
-    else if (maxChar > 100)
+    else if (maxLine > 100)
     {
         printf("Highest character count must be less than 100\n");
         exit(-1400);
     }
-    // Open file.  exit with error code if error.
+    
+    FILE *f;
+    
+    char c;
+    
     f = fopen("test.txt", "r");
     if (f == NULL)
     {
@@ -144,7 +186,6 @@ int main(int argc, char *argv[])
         exit(-1000);
     }
     
-    maxChar++; // needs one for the null character
     char lineCantonese[50][100];
     char lineEnglish[50][100];
     char string[100];
